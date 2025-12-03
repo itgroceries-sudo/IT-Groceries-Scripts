f199c40e046a9d6c905abc582fc2875787cc1113
itgroceries-sudo <itgroceries@gmail.com>
2025-12-03 11:54:03 +0700

    Firefox isnt_03.ps1

---
+++ scripts/isnt_03.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = 'Stop'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+try {
+    Write-Host "[ CLOUD ] Downloading Firefox..." -ForegroundColor Cyan
+    $url = "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"
+    $dest = "$env:TEMP\firefox_setup.exe"
+    
+    Invoke-WebRequest -Uri $url -OutFile $dest
+    
+    if (Test-Path $dest) {
+        Write-Host "[ CLOUD ] Installing..." -ForegroundColor Green
+        Start-Process -FilePath $dest -ArgumentList "/S" -Wait
+        Remove-Item $dest -Force
+        exit 0
+    } else { throw "Download failed." }
+} catch {
+    Write-Host "[ ERROR ] $_" -ForegroundColor Red
+    exit 1
+}
