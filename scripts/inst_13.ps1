eb89ebebf62bcb8378d632df589bb38a5a10c389
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:51:35 +0700

    Update inst_13.ps1

--- scripts/inst_13.ps1
+++ scripts/inst_13.ps1
@@ -1,23 +1,5 @@
-$ErrorActionPreference = 'Stop'
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$url = "https://github.com/audacity/audacity/releases/download/Audacity-3.7.0/audacity-win-3.7.0-64bit.exe"
+$fileName = "Audacity_Setup.exe"
+$installArgs = "/VERYSILENT"
 
-try {
-    Write-Host "[ CLOUD ] Checking Audacity Github..." -ForegroundColor Cyan
-    $api = "https://api.github.com/repos/audacity/audacity/releases/latest"
-    $json = Invoke-RestMethod -Uri $api
-    $asset = $json.assets | Where-Object {$_.name -like "*64bit.exe"} | Select-Object -First 1
-    
-    if ($asset) {
-        $dest = "$env:TEMP\audacity_setup.exe"
-        Write-Host "Downloading $($asset.name)..." -ForegroundColor Yellow
-        Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $dest
-        
-        Write-Host "[ CLOUD ] Installing..." -ForegroundColor Green
-        Start-Process -FilePath $dest -ArgumentList "/VERYSILENT" -Wait
-        Remove-Item $dest -Force
-        exit 0
-    } else { throw "Asset not found." }
-} catch {
-    Write-Host "[ ERROR ] $_" -ForegroundColor Red
-    exit 1
-}
+. "$env:TEMP\Master.ps1"
