9d7d1a32d806f066329a8cef0581cc07dc350a78
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 19:42:20 +0700

    Update inst_02.ps1

--- scripts/inst_02.ps1
+++ scripts/inst_02.ps1
@@ -1,5 +1,9 @@
-$url = "https://dl.google.com/chrome/install/standalonesetup64.exe"
-$fileName = "ChromeSetup.exe"
-$installArgs = "/silent /install"
+# เปลี่ยนมาใช้ MSI เพื่อแก้ปัญหาติดตั้งไม่สมบูรณ์
+$url = "https://dl.google.com/chrome/install/googlechromestandaloneenterprise64.msi"
+$fileName = "ChromeSetup.msi"
+
+# Master Engine จะรู้เองว่าเป็น MSI แล้วใช้ msiexec /i ให้
+# เราใส่แค่คำสั่ง Silent ก็พอ
+$installArgs = "/qn /norestart"
 
 . "$env:TEMP\Master.ps1"
