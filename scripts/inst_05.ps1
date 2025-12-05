26e5cb65d35b014c181c0414501b3003974f941c
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:52:36 +0700

    Update inst_05.ps1

--- scripts/inst_05.ps1
+++ scripts/inst_05.ps1
@@ -1,7 +1,5 @@
-# ==============================================================================================
-#  FILE: inst_05.ps1 (Notepad++)
-# ==============================================================================================
 $url = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.7.1/npp.8.7.1.Installer.x64.exe"
 $fileName = "npp_setup.exe"
 $installArgs = "/S"
+
 . "$env:TEMP\Master.ps1"
