a12aa4d265d0ba580c151f474868800421b7fc0c
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:50:43 +0700

    Update inst_10.ps1

--- scripts/inst_10.ps1
+++ scripts/inst_10.ps1
@@ -1,3 +1,4 @@
 $url = "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2400420272/AcroRdrDC2400420272_en_US.exe"
+$fileName = "DiscordSetup.exe"
 $installArgs =  "/sAll /rs /msi EULA_ACCEPT=YES"
 . "$env:TEMP\Master.ps1"
