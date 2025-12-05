d92d4853a40e38df3955339ecbf031d199855c47
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:55:24 +0700

    Update inst_16.ps1

--- scripts/inst_16.ps1
+++ scripts/inst_16.ps1
@@ -1,20 +1,5 @@
-$ErrorActionPreference = 'Stop'
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$url = "https://download.cpuid.com/cpu-z/cpu-z_2.12-en.exe"
+$fileName = "cpuz_setup.exe"
+$installArgs = "/SILENT"
 
-try {
-    Write-Host "[ CLOUD ] Downloading CPU-Z..." -ForegroundColor Cyan
-    $url = "https://download.cpuid.com/cpu-z/cpu-z_2.12-en.exe"
-    $dest = "$env:TEMP\cpuz_setup.exe"
-    
-    Invoke-WebRequest -Uri $url -OutFile $dest
-    
-    if (Test-Path $dest) {
-        Write-Host "[ CLOUD ] Installing..." -ForegroundColor Green
-        Start-Process -FilePath $dest -ArgumentList "/SILENT" -Wait
-        Remove-Item $dest -Force
-        exit 0
-    } else { throw "Download failed." }
-} catch {
-    Write-Host "[ ERROR ] $_" -ForegroundColor Red
-    exit 1
-}
+. "$env:TEMP\Master.ps1"
