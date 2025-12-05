3052b1a6b607e7a212fed18b02fa2f454f63f78d
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:55:08 +0700

    Update inst_11.ps1

--- scripts/inst_11.ps1
+++ scripts/inst_11.ps1
@@ -1,20 +1,5 @@
-$ErrorActionPreference = 'Stop'
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$url = "https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe"
+$fileName = "TeamViewer_Setup.exe"
+$installArgs = "/S"
 
-try {
-    Write-Host "[ CLOUD ] Downloading TeamViewer..." -ForegroundColor Cyan
-    $url = "https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe"
-    $dest = "$env:TEMP\tv_setup.exe"
-    
-    Invoke-WebRequest -Uri $url -OutFile $dest
-    
-    if (Test-Path $dest) {
-        Write-Host "[ CLOUD ] Installing..." -ForegroundColor Green
-        Start-Process -FilePath $dest -ArgumentList "/S" -Wait
-        Remove-Item $dest -Force
-        exit 0
-    } else { throw "Download failed." }
-} catch {
-    Write-Host "[ ERROR ] $_" -ForegroundColor Red
-    exit 1
-}
+. "$env:TEMP\Master.ps1"
