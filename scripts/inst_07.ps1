0218b4e0655171e2c43484beb3513f6793733b45
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:54:19 +0700

    Update inst_07.ps1

--- scripts/inst_07.ps1
+++ scripts/inst_07.ps1
@@ -1,7 +1,5 @@
-# ==============================================================================================
-#  FILE: inst_07.ps1 (Zoom Meeting - MSI)
-# ==============================================================================================
 $url = "https://zoom.us/client/latest/ZoomInstallerFull.msi"
 $fileName = "ZoomInstaller.msi"
 $installArgs = "/qn"
+
 . "$env:TEMP\Master.ps1"
