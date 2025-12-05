cc26367108aa9c02e79e24b4d4481cf726d24881
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:41:54 +0700

    Update inst_03.ps1

--- scripts/inst_03.ps1
+++ scripts/inst_03.ps1
@@ -1,6 +1,5 @@
-# ==============================================================================================
-#  FILE: inst_03.ps1 (Mozilla Firefox)
-# ==============================================================================================
 $url = "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"
 $fileName = "FirefoxSetup.exe"
 $installArgs = "-ms"
+
+. "$env:TEMP\Master.ps1"
