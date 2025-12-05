ff6c30382d76269d691e30c35022578dd215ea91
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:54:03 +0700

    Update inst_06.ps1

--- scripts/inst_06.ps1
+++ scripts/inst_06.ps1
@@ -1,6 +1,6 @@
-# ==============================================================================================
-#  FILE: inst_06.ps1 (AnyDesk)
-# ==============================================================================================
 $url = "https://download.anydesk.com/AnyDesk.exe"
 $fileName = "AnyDesk.exe"
+# AnyDesk ต้องการ path ปลายทางชัดเจนสำหรับการติดตั้งแบบเงียบ
+$installArgs = "--install `"$env:ProgramFiles(x86)\AnyDesk`" --start-with-win --silent"
+
 . "$env:TEMP\Master.ps1"
