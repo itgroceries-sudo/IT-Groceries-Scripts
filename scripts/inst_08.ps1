3262d471817d9e2eabde4cf504d4b7227e37c7f7
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:54:38 +0700

    Update inst_08.ps1

--- scripts/inst_08.ps1
+++ scripts/inst_08.ps1
@@ -1,7 +1,6 @@
-# ==============================================================================================
-#  FILE: inst_08.ps1 (Discord)
-# ==============================================================================================
 $url = "https://discord.com/api/download?platform=win"
 $fileName = "DiscordSetup.exe"
+# Discord ใช้ Squirrel Installer (ติดตั้งลง User Profile อัตโนมัติ)
 $installArgs = "-s"
+
 . "$env:TEMP\Master.ps1"
