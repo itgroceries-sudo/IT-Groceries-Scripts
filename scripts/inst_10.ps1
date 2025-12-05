ce8e3b2427441566768a8001b7cdb92734c4f1e7
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:48:54 +0700

    Update inst_10.ps1

--- scripts/inst_10.ps1
+++ scripts/inst_10.ps1
@@ -1,3 +1,3 @@
-    $url = "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2400420272/AcroRdrDC2400420272_en_US.exe"
-        Start-Process -FilePath $dest -ArgumentList "/sAll /rs /msi EULA_ACCEPT=YES" -Wait
-    . "$env:TEMP\Master.ps1"
+$url = "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2400420272/AcroRdrDC2400420272_en_US.exe"
+$installArgs =  "/sAll /rs /msi EULA_ACCEPT=YES"
+. "$env:TEMP\Master.ps1"
