054f2185f8b307a4427a2c634f8239fd48492a62
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:41:19 +0700

    Update inst_02.ps1

--- scripts/inst_02.ps1
+++ scripts/inst_02.ps1
@@ -1,6 +1,5 @@
-# ==============================================================================================
-#  FILE: inst_02.ps1 (Google Chrome - Enterprise Standalone)
-# ==============================================================================================
 $url = "https://dl.google.com/chrome/install/standalonesetup64.exe"
 $fileName = "ChromeSetup.exe"
 $installArgs = "/silent /install"
+
+. "$env:TEMP\Master.ps1"
