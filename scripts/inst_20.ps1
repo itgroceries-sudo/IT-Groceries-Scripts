e8f13b6d583751c59819761b1a66944d5d889a57
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:55:51 +0700

    Update inst_20.ps1

--- scripts/inst_20.ps1
+++ scripts/inst_20.ps1
@@ -1,20 +1,5 @@
-$ErrorActionPreference = 'Stop'
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$url = "https://www.voidtools.com/Everything-1.4.1.1026.x64-Setup.exe"
+$fileName = "Everything_Setup.exe"
+$installArgs = "/S"
 
-try {
-    Write-Host "[ CLOUD ] Downloading Everything..." -ForegroundColor Cyan
-    $url = "https://www.voidtools.com/Everything-1.4.1.1026.x64-Setup.exe"
-    $dest = "$env:TEMP\everything_setup.exe"
-    
-    Invoke-WebRequest -Uri $url -OutFile $dest
-    
-    if (Test-Path $dest) {
-        Write-Host "[ CLOUD ] Installing..." -ForegroundColor Green
-        Start-Process -FilePath $dest -ArgumentList "/S" -Wait
-        Remove-Item $dest -Force
-        exit 0
-    } else { throw "Download failed." }
-} catch {
-    Write-Host "[ ERROR ] $_" -ForegroundColor Red
-    exit 1
-}
+. "$env:TEMP\Master.ps1"
