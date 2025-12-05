e726d232fe79ac7a6d6cbff3228e2f417edf57f5
itgroceries-sudo <itgroceries@gmail.com>
2025-12-05 18:55:38 +0700

    Update inst_19.ps1

--- scripts/inst_19.ps1
+++ scripts/inst_19.ps1
@@ -1,23 +1,5 @@
-$ErrorActionPreference = 'Stop'
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$url = "https://github.com/microsoft/PowerToys/releases/download/v0.86.0/PowerToysSetup-0.86.0-x64.exe"
+$fileName = "PowerToysSetup.exe"
+$installArgs = "/install /quiet"
 
-try {
-    Write-Host "[ CLOUD ] Checking PowerToys Github..." -ForegroundColor Cyan
-    $api = "https://api.github.com/repos/microsoft/PowerToys/releases/latest"
-    $json = Invoke-RestMethod -Uri $api
-    $asset = $json.assets | Where-Object {$_.name -like "*-x64.exe"} | Select-Object -First 1
-    
-    if ($asset) {
-        $dest = "$env:TEMP\ptoys_setup.exe"
-        Write-Host "Downloading $($asset.name)..." -ForegroundColor Yellow
-        Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $dest
-        
-        Write-Host "[ CLOUD ] Installing..." -ForegroundColor Green
-        Start-Process -FilePath $dest -ArgumentList "/install /quiet /norestart" -Wait
-        Remove-Item $dest -Force
-        exit 0
-    } else { throw "Asset not found." }
-} catch {
-    Write-Host "[ ERROR ] $_" -ForegroundColor Red
-    exit 1
-}
+. "$env:TEMP\Master.ps1"
