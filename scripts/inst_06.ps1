5f0bb4040463c76f86605f61c2935e72a656adf1
itgroceries-sudo <itgroceries@gmail.com>
2025-12-06 10:59:07 +0700

    Update inst_06.ps1

--- scripts/inst_06.ps1
+++ scripts/inst_06.ps1
@@ -1,6 +1,6 @@
 # AnyDesk (Fixed Arguments)
 $url = "https://download.anydesk.com/AnyDesk.msi"
-$fileName = "AnyDesk.exe"
+$fileName = "AnyDesk.msi"
 
 # [FIX] ไม่ระบุ path ปลายทาง (ให้มันลง Default เอง) เพื่อป้องกันปัญหาเรื่อง Quote ใน PowerShell
 # ใช้คำสั่ง --install --start-with-win --silent ตามมาตรฐาน
